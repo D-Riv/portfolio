9f6754425be8ad745eaf79b46ac91be5cd1be9c3
Dennis Rivera <rivd19@outlook.com>
2020-04-23 13:57:19 -0400

    Added introduction text to the home page with type writer effect

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,46 @@
 import React from "react";
 import Layout from "../components/Layout";
+import Typical from "react-typical";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <Layout>
-      <h1>Hello!</h1>
+      <div className="header-container">
+        <div className="header-wrapper">
+          <p className="header-title">
+            Hi,
+            <br /> Im <span className="name">Dennis Rivera</span> 🤙🏽
+            <br />I am a{" "}
+            <Typical
+              loop={Infinity}
+              wrapper="b"
+              steps={[
+                "Software Developer 💻",
+                1000,
+                "USMC Veteran 🇺🇸",
+                1000,
+                "Husband 💍",
+                1000,
+                "Gymrat 💪🏽",
+                1000,
+              ]}
+            />
+          </p>
+        </div>
+        <div className="header-button-container">
+          <Link to="/about">
+            <button className="info-button">Get to Know Me</button>
+          </Link>
+
+          <Link to="/portfolio">
+            <button className="info-button">Check Out My Work</button>
+          </Link>
+        </div>
+        <div className="coding-image">
+          <i class="fas fa-code"></i>
+        </div>
+      </div>
     </Layout>
   );
 };
