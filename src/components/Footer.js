1b6099804696b8d315b0d41ba619db9cf6d6ba8c
Dennis Rivera <rivd19@outlook.com>
2020-04-23 13:50:11 -0400

    Added the footer component to the layout component

---
+++ src/components/Footer.js
@@ -0,0 +1,79 @@
+import React from "react";
+import styled from "styled-components";
+
+const FooterWrapper = styled.div`
+  margin: 0 5rem;
+  max-height: 300px;
+`;
+
+const FooterContainer = styled.div`
+  border-top: 1px solid #000000;
+  text-align: center;
+`;
+
+const FooterUl = styled.ul`
+  display: flex;
+  justify-content: center;
+`;
+
+const FooterLi = styled.li`
+  font-size: 30px;
+  cursor: pointer;
+  margin: 10px 20px;
+  list-style: none;
+  transition: transform 0.2s;
+
+  :hover {
+    transform: scale(1.3);
+  }
+`;
+
+const FooterText = styled.h3`
+  font-family: "Montserrat", sans-serif;
+  font-weight: 300;
+  font-size: 15px;
+  color: #575757;
+`;
+
+const Footer = () => {
+  return (
+    <FooterWrapper>
+      <FooterContainer>
+        <FooterUl>
+          <FooterLi className="footer-li">
+            <a href="https://github.com/D-Riv" target="_blank">
+              <i class="fab fa-github-square"></i>
+            </a>
+          </FooterLi>
+          <FooterLi className="footer-li">
+            <a
+              href="https://www.linkedin.com/in/dennis-rivera/"
+              target="_blank"
+            >
+              <i class="fab fa-linkedin"></i>
+            </a>
+          </FooterLi>
+          <FooterLi className="footer-li">
+            <a href="https://www.instagram.com/d.rivs/?hl=en" target="_blank">
+              <i class="fab fa-instagram-square"></i>
+            </a>
+          </FooterLi>
+          <FooterLi className="footer-li">
+            <a
+              href="https://www.facebook.com/dennis.rivera.988926"
+              target="_blank"
+            >
+              <i class="fab fa-facebook-square"></i>
+            </a>
+          </FooterLi>
+        </FooterUl>
+
+        <FooterText>
+          Copyright <i class="far fa-copyright"></i> 2020, Dennis
+        </FooterText>
+      </FooterContainer>
+    </FooterWrapper>
+  );
+};
+
+export default Footer;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,10 @@
 import React from "react";
 import Header from "./Header";
-// import Footer from "./footer";
+import Footer from "./Footer";
 // import "../styles/styles.css";
 import styled from "styled-components";
 
 const LayoutContainer = styled.div`
-  // margin: 0 auto;
-  // max-width: 100rem;
-  // padding: 1rem;
   display: flex;
   flex-direction: column;
   min-height: 100vh;
@@ -20,20 +17,11 @@ const LayoutContent = styled.div`
 const Layout = (props) => {
   return (
     <LayoutContainer>
-      <link
-        href="https:////use.fontawesome.com/releases/v5.12.1/css/all.css"
-        rel="stylesheet"
-        crossOrigin="anonymous"
-      />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
-        rel="stylesheet"
-      ></link>
       <LayoutContent>
         <Header title={props.title} description={props.description} />
         {props.children}
       </LayoutContent>
-      {/* <Footer /> */}
+      <Footer />
     </LayoutContainer>
   );
 };
